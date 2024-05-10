b77166c76e98fb038681f16c02350967a04c6a8e
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-05-10 15:29:29 +0200

    print in page with vue js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
+//creo constante per la funzione vue e gli dico che è uguale alla classe vue
 const { createApp } = Vue
-
-  createApp({
-    data() {
-      return {
-        
-      }
+//creo funzione che mi ritorna per adesso dei dati e in futuro metodi che contengono funzioni ecc.
+createApp({
+  data() {
+    return {
+        welcomeMessage : "Ciao Benvenuto su Vue.js",
+        helpMessage : "Da oggi sarò il tuo aiuto principale su Js",
+        message : "Oggi aiuterò?",
+        firstName : "",
+        lastName : ""
     }
-  }).mount('#app')
+  }
+  //monto o inietto quei dati nel div con id app dove andrò a scrivere tutto il codice html
+}).mount('#app')
